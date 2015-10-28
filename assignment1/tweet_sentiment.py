5db8c54ff1ad91b93c5cf0c78fcd51565c9f6c02
Theo Lekkas <theofanis.lekkas@theofanislekkas.com>
2015-10-28 14:55:55 -0500

    First attempt.

--- assignment1/tweet_sentiment.py
+++ assignment1/tweet_sentiment.py
@@ -1,16 +1,41 @@
 import json
 import sys
 
-def hw():
-    print 'Hello, world!'
+
+reload(sys)
+sys.setdefaultencoding("utf-8")
+
+sent_file = open(sys.argv[1])
+tweet_file = open(sys.argv[2])
+
+scores = {}
+tweet_dict = []
+
+for tweet in tweet_file:
+    tweet_dict.append(json.loads(tweet))
+# return tweet_dict
+
+for x in tweet_dict:
+    """
+    As is, instead of scoring each line & printing out, this loop 
+    (w/in a loop) is printing out each value from 0 to 6 in order...
+    No idea why that is happening.  I think it's sorting the returned
+    values.
+    """
+    if x.get('text'):
+        tscore = 0
+        for line in sent_file:
+            term, score = line.split("\t")
+            scores['term'] = int(score)
+            if term in x.get('text'):
+                tscore = tscore + scores['term']
+            print tscore
+
 
 def lines(fp):
     print str(len(fp.readlines()))
 
 def main():
-    sent_file = open(sys.argv[1])
-    tweet_file = open(sys.argv[2])
-    hw()
     lines(sent_file)
     lines(tweet_file)
 
