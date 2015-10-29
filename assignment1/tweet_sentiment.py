3cb7f90c63eee5a7de5a8a1f9fd440c192c58900
Theo Lekkas <theofanis.lekkas@theofanislekkas.com>
2015-10-29 14:19:49 -0500

    Successfully implemented.

--- assignment1/tweet_sentiment.py
+++ assignment1/tweet_sentiment.py
@@ -1,45 +1,43 @@
 import json
+import re
 import sys
 
 
-reload(sys)
-sys.setdefaultencoding("utf-8")
+def senti_file(sent_file):
+    scores = {}
+    for line in sent_file:
+        term, score = line.split("\t")
+        scores[term] = int(score)
+    
+    return scores
 
-sent_file = open(sys.argv[1])
-tweet_file = open(sys.argv[2])
+def tweets(tweet_file):
+    tweet_list = []
+    for tweet in tweet_file:
+        tweet_list.append(json.loads(tweet))
 
-scores = {}
-tweet_dict = []
+    return tweet_list
 
-for tweet in tweet_file:
-    tweet_dict.append(json.loads(tweet))
-# return tweet_dict
-
-for x in tweet_dict:
-    """
-    As is, instead of scoring each line & printing out, this loop 
-    (w/in a loop) is printing out each value from 0 to 6 in order...
-    No idea why that is happening.  I think it's sorting the returned
-    values.
-    """
-    if x.get('text'):
-        tscore = 0
-        for line in sent_file:
-            term, score = line.split("\t")
-            scores['term'] = int(score)
-            if term in x.get('text'):
-                tscore = tscore + scores['term']
+def sentiment(tweet_list, scores):
+    for x in tweet_list:
+        if x.get('text'):
+            tscore = 0
+            text = x.get('text').encode("ascii", "ignore")
+            bag = text.split()
+            for term in scores:
+                if term in bag:
+                    # import ipdb;ipdb.set_trace()
+                    sent = bag.count(term)
+                    tscore += sent*scores[term]
             print tscore
 
-# I want to call a function that adds up the sentiment w/in a loop
-# that grabs each tweet['text'], not the way I'm doing it above.
-
-def lines(fp):
-    print str(len(fp.readlines()))
-
 def main():
-    lines(sent_file)
-    lines(tweet_file)
+    sent_file = open(sys.argv[1])
+    tweet_file = open(sys.argv[2])
+    scores = senti_file(sent_file)
+    tweet_list = tweets(tweet_file)
+    sentiment(tweet_list, scores)
+
 
 if __name__ == '__main__':
     main()
