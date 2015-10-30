d2a71d337df749997a766355fbe5620b8d9664ad
Theo Lekkas <theofanis.lekkas@theofanislekkas.com>
2015-10-30 11:14:28 -0500

    Write up some pseudocode.

--- assignment1/term_sentiment.py
+++ assignment1/term_sentiment.py
@@ -16,7 +16,33 @@ Assign some type of sentiment value to the non-AFINN terms.
 
 Return the term & it's value ('term' float(12.03))
 """
+tweet_sentiment funcs
 
+def filtered_list(filtered_list):
+    #for term in filtered_list:
+        #new_sent = []
+        #if sentiment != 0:
+        #    new_sent.append(item)
+    #return new_sent
+    pass
+
+def repeated_terms(new_sent):
+    #repeated_terms = []
+    #Look for terms that are repeated
+    #create a new list of tweets with repeted terms
+    #return repeated_terms
+    pass
+
+def new_sentiment(repeated_terms):
+    #if sentiment > 0:
+    #    total = new_term.count()
+    #    new_sent_score = total*some_value
+    #else:
+    #    if sentiment < 0:
+    #        total = new_term.count()
+        #    new_sent_score = total*some_value
+    #return new_term, new_score
+    pass
 
 def lines(fp):
     print str(len(fp.readlines()))
