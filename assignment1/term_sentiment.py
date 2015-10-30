1090a043f290ae7d80e90bcd35103218ddc6d4ac
Theo Lekkas <theofanis.lekkas@theofanislekkas.com>
2015-10-30 10:57:03 -0500

    Update file with some notes breaking down the problem.

--- assignment1/term_sentiment.py
+++ assignment1/term_sentiment.py
@@ -1,6 +1,15 @@
 import sys
 
 
+"""
+Search both the AFINN file & the twitter file.
+Look for AFINN terms in each tweet.
+For tweets with AFINN terms look for additional terms that are repeated
+with the AFINN term.
+Assign some type of sentiment value to the non-AFINN terms.
+Return the term & it's value ('term' float(12.03))
+"""
+
 
 def lines(fp):
     print str(len(fp.readlines()))
