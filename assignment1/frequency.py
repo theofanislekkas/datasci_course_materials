a282b138b43e821115b86d85ca479c3c30774f2e
Theo Lekkas <theofanis.lekkas@theofanislekkas.com>
2015-11-19 12:29:12 -0600

    Change to a straight dict instead list of dicts. This will remove duplicates.

--- assignment1/frequency.py
+++ assignment1/frequency.py
@@ -11,7 +11,7 @@ def tweets(tweet_file):
     return tweet_list
 
 def tweet_count(tweet_list):
-    count_list = []
+    count_list = {}
     for x in tweet_list:
         term = ''
         count = 0
@@ -19,24 +19,21 @@ def tweet_count(tweet_list):
             text = x.get('text').encode("ascii", "ignore")
             exclude = set(string.punctuation + string.digits)
             temp = ''.join(c for c in text if c not in exclude)
-            bag = temp.split()
+            bag = temp.split().lower()
+            bag.lower()
             for term in bag:
                 count += 1
         if count != 0:
-            if term in count_list:
-                print term
-            # import  ipdb;ipdb.set_trace()
-            count_list.append({term: float(count)})
+            count_list.update({term: float(count)})
         
     return count_list
 
 def term_freq(tweet_counts):
     total_term = len(tweet_counts)
     for i in tweet_counts:
-        for k in i:
-            value = i[k]
-            term_count = value/total_term
-            print k, term_count
+        value = tweet_counts.get(i)
+        term_count = value/total_term
+        print i, term_count
     
 
 def main():
